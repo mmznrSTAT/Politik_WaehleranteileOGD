7b72d8caa21996edfafb0d39f4aa60a70385177d
Mazenauer Matthias <B105PMM@ji.ktzh.ch>
2019-03-21 17:44:20 +0100

    added wahlbeteiligung

--- script.R
+++ script.R
@@ -9,26 +9,26 @@ library(jsonlite)
 library(dplyr)
 library(RJSONIO)
 
+#####################
+##  Wähleranteile  ##
+#####################
 json_file <- "https://www.wahlen.zh.ch/wahlen2019/public/data/krw/listengde.json"
 json_data <- jsonlite::fromJSON(json_file)
-json_data
 
 json_file1 <- "https://www.wahlen.zh.ch/wahlen2019/public/data/krw/listen.json"
 json_data1 <- jsonlite::fromJSON(json_file1)
-json_data1
 
 
-##json_data_frame <- as.data.frame(json_data)
+
+
 flat1 <- flatten(json_data$listen_auf_gemeindeebene)
 flat2 <- flatten(json_data$listen_stadtzh)
 flat3 <- flatten(json_data1$listen_auf_kantonsebene)
-
 
 flat2$wahlkreis_nr <- NA
 flat2$wahlkreis_bez <- NA
 flat2$gemeinde_bfsnr <- 261
 flat2$gemeinde_bez <- "Kanton Zürich"
-
 
 flat1$stimmenzusatz <- NULL
 
@@ -40,10 +40,51 @@ flat2$letzte_wahl_waehlerproz_zr <- NULL
 flat2$gewinn_waehlerproz_zr <- NULL
 flat2$letzte_wahl_sitze <- NULL
 
-
 flat <- rbind(flat1,flat2)
 
-
 write.csv(flat, file = "Waehleranteile_KRW19_Gemeinden.csv")
-
 write.csv(flat3, file = "Waehleranteile_KRW19_Kanton.csv")
+
+
+#####################
+## Wahlbeteiligung ##
+#####################
+json_fileWB <- "https://www.wahlen.zh.ch/wahlen2019/public/data/krw/wahlbeteiligung.json"
+json_wahlbet <- jsonlite::fromJSON(json_fileWB)
+
+
+#Gemeindeebene
+head(json_wahlbet$wahlbeteiligung_auf_gemeindeebene)
+head(json_wahlbet$wahlbeteiligung_stadtzh)
+
+wahlbetGem <- flatten(json_wahlbet$wahlbeteiligung_auf_gemeindeebene)
+
+#Hier das gleiche machen wie bei Kanton, da nur ein Object
+
+#dann flatten
+wahlbetStadtZH <- flatten(json_wahlbet$wahlbeteiligung_stadtzh)
+wahlbetStadtZH$gemeinde_bfsnr <- 261
+
+wahlbetGem %>%
+    select(gemeinde_bfsnr,gemeinde_bez,wahlbeteiligung,letzte_wahl_wahlbeteiligung)
+
+wahlbetStadtZH %>%
+  select(gemeinde_bfsnr,gemeinde_bez,wahlbeteiligung,letzte_wahl_wahlbeteiligung)
+
+wahlbetGem <- rbind(wahlbetGem,wahlbetStadtZH)
+
+
+
+#Kantonsebene
+kanton <- json_wahlbet$wahlbeteiligung_auf_kantonsebene
+kanton <- data.frame(matrix(unlist(kanton)))
+                     
+kanton <- jsonlite::fromJSON(json_wahlbet$wahlbeteiligung_auf_kantonsebene)
+wahlbetZH <- flatten(kanton)
+
+
+
+write.csv(wahlbetZH, file = "wahlbeteiligung_KRW2019_Kanton.csv")
+write.csv(wahlbetGem, file = "wahlbeteiligung_KRW2019_Gemeinden.csv")
+
+
