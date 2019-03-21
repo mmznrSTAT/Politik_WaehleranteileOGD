c1c429cd35835eb6818ae8e6fd7fb245901e15ff
Mazenauer Matthias <B105PMM@ji.ktzh.ch>
2019-03-21 16:28:20 +0100

    minor code

--- script.R
+++ script.R
@@ -17,11 +17,11 @@ json_file1 <- "https://www.wahlen.zh.ch/wahlen2019/public/data/krw/listen.json"
 json_data1 <- jsonlite::fromJSON(json_file1)
 json_data1
 
+
 ##json_data_frame <- as.data.frame(json_data)
 flat1 <- flatten(json_data$listen_auf_gemeindeebene)
 flat2 <- flatten(json_data$listen_stadtzh)
 flat3 <- flatten(json_data1$listen_auf_kantonsebene)
-head(flat)
 
 
 flat2$wahlkreis_nr <- NA
@@ -29,14 +29,6 @@ flat2$wahlkreis_bez <- NA
 flat2$gemeinde_bfsnr <- 261
 flat2$gemeinde_bez <- "Kanton Zürich"
 
-
-head(flat3)
-
-
-flat2$wahlkreis_nr <- NA
-flat2$wahlkreis_bez <- NA
-flat2$gemeinde_bfsnr <- 261
-flat2$gemeinde_bez <- "Zürich"
 
 flat1$stimmenzusatz <- NULL
 
@@ -48,11 +40,10 @@ flat2$letzte_wahl_waehlerproz_zr <- NULL
 flat2$gewinn_waehlerproz_zr <- NULL
 flat2$letzte_wahl_sitze <- NULL
 
-head(flat1)
-head(flat2)
-
-
 
 flat <- rbind(flat1,flat2)
 
+
 write.csv(flat, file = "Waehleranteile_KRW19_Gemeinden.csv")
+
+write.csv(flat3, file = "Waehleranteile_KRW19_Kanton.csv")
